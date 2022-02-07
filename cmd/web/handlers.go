a870bb20c4f60c15b8d5771bb4f07473d843428c
mrohadi <muhammadrohadi03@gmail.com>
2022-02-08 01:30:31 +0700

    Project structure and organization

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,33 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func main() {
-	// Use the http.NewServeMux() function to initialize new servemux, then
-	// register the home handler function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", homeHandler)
-	
-	// Register the two new handler functions and corresponding URL pattern
-	mux.HandleFunc("/snippet", showSnippetHandler)
-	mux.HandleFunc("/snippet/create", createSnippetHandler)
-	
-	// Use the http.ListenAndServe() function to start a new web server. We pass
-	// two parameters: the TCP network addesss to listen on (in this case ":4000")
-	// and the servermux instance we just created. If the http.ListenAndServe()
-	// return an error we use log.Fatal to log an error message and exit.
-	log.Println("Starting server on: 4000")	
-	err := http.ListenAndServe(":4000", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-//homeHandler define a home handler function which writes a byte slice containing 
+//homeHandler define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// Check if the current URL path exactly matches "/". If it doesn't 
