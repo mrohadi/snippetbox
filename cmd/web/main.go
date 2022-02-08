b34be0846b189648abf2c5c429bcd7182c39d307
mrohadi <muhammadrohadi03@gmail.com>
2022-02-08 22:27:11 +0700

    Managing configuration setting

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define a new command-line flag with the name 'addr', a default value of
+	// and some short help text explaining what the flag controls. The value of
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP networkd address")
+	
+	// Importantly, we use the flag.Parse() function to parse the command line 
+	// This read in the command-line flag value and assign it to the addr variable.
+	// You need to call this *before* you use addr variable, otherwise it will 
+	// always contain the default value of ":4000". If any error ecountered
+	// during parsing, the application will be terminated.
+	flag.Parse()
+
 	// Use the http.NewServeMux() function to initialize new servemux, then
 	// register the home handler function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
@@ -29,8 +42,8 @@ func main() {
 	// two parameters: the TCP network addesss to listen on (in this case ":4000")
 	// and the servermux instance we just created. If the http.ListenAndServe()
 	// return an error we use log.Fatal to log an error message and exit.
-	log.Println("Starting server on: 4000")	
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on: %s\n", *addr)	
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
