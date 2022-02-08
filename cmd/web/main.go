f870fe1c31fe650caec6354072c8f6885419bc5b
mrohadi <muhammadrohadi03@gmail.com>
2022-02-09 01:59:40 +0700

    Creating a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Define an application struct to hold the application-wide dependencies for the
@@ -17,10 +20,23 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP networkd address")
+	dns := flag.String("dns", "mrohadi:@Adiganteng123@/snippetbox?parseTime=true", "MySQL database connection")
 	flag.Parse()
 	
 	infoLog := log.New(os.Stdout, "INFO - ", log.Ldate | log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR - ", log.Ldate | log.Ltime | log.Lshortfile)
+	
+	// To keep the main() function tidy I've put the code for creating a connec
+	// pool into the separate openDB() function below. We pass openDB() the DSN
+	// from the command-line flag.
+	db, err := openDB(*dns)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
 	
 	// Initialize a new instance of application containing the dependencies.
 	app := &application {
@@ -35,9 +51,24 @@ func main() {
 	}
 	
 	infoLog.Printf("Starting server on: %s\n", *addr)
-	
-	err := svr.ListenAndServe()
+	err = svr.ListenAndServe()
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool
+// for a given DSN.
+func openDB(dns string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dns)	
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	
+	return db, nil
 }
