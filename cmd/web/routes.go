662dd4110ab43c022e1d95d873d845350f126837
mrohadi <muhammadrohadi03@gmail.com>
2022-02-12 00:58:00 +0700

    Setting security header

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,14 +2,14 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.homeHandler)	
+	mux.HandleFunc("/", app.homeHandler)
 	mux.HandleFunc("/snippet", app.showSnippetHandler)
 	mux.HandleFunc("/snippet/create", app.createSnippetHandler)
-	
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", fileServer)
-	
-	return mux
+
+	return secureHeader(mux)
 }
