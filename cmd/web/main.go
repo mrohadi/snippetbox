91d7847ae834c5c33f89a2503aca85b5501cd484
mrohadi <muhammadrohadi03@gmail.com>
2022-02-12 21:16:46 +0700

    Connection timeouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -73,10 +73,13 @@ func main() {
 	}
 
 	svr := &http.Server{
-		Addr:      *addr,
-		ErrorLog:  errorLog,
-		Handler:   app.routes(),
-		TLSConfig: tlsConfig,
+		Addr:         *addr,
+		ErrorLog:     errorLog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("Starting server on: %s\n", *addr)
